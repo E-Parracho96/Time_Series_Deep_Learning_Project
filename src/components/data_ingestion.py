72559dcaaf96145b0ce6c64819ac64cf3843a0f4
miguel.parracho <miguel.parracho@global.com>
2023-10-02 09:13:54 +0100

    - added params to main() - tidied the code and removed unused imports - started wworking on tests for data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,20 +1,19 @@
-import os
 import sys
 import pandas as pd
-import numpy as np
 from src.exception import CustomException
 from src.logger import logging
 
 
-def organize_file (df):
-    df = df.rename(columns = {'producao_total':'production_PV','consumo_rede':'consumption','injecao_rede':'grid_injection'}) # rename columns
-    df['event_timestamp'] = pd.to_datetime(df['event_timestamp'], unit='s') # Converting to datetime
-    df = df.sort_values(by=['event_timestamp']).reset_index(drop=True) # Sorting the data by datetime
+def organize_file(df):
+    df = df.rename(columns={'producao_total': 'production_PV', 'consumo_rede': 'consumption',
+                            'injecao_rede': 'grid_injection'})
+    df['event_timestamp'] = pd.to_datetime(df['event_timestamp'], unit='s')
+    df = df.sort_values(by=['event_timestamp']).reset_index(drop=True)
 
     return df
 
-def data_ingestion(file1_path, file2_path):
 
+def data_ingestion(file1_path, file2_path):
     """This function read the house file, the sun_position_PT file and join them."""
     try:
         logging.info('Read the house dataset as dataframe')
@@ -24,29 +23,24 @@ def data_ingestion(file1_path, file2_path):
         column_names = ['event_timestamp', 'producao_total']
         missing_columns = [col for col in column_names if col not in df1.columns]
         if missing_columns:
-            raise ValueError(f"The following essential columns are missing in the DataFrame: {', '.join(missing_columns)}")
-        
+            raise ValueError(
+                f"The following essential columns are missing in the DataFrame: {', '.join(missing_columns)}")
+
         # Check if producao_total value is float type an if event_timestamp is int
-        if  df1['producao_total'].dtype != float and df1['event_timestamp'].dtype != int:
+        if df1['producao_total'].dtype != float and df1['event_timestamp'].dtype != int:
             raise ValueError("Error in column type.")
 
         df_1 = organize_file(df1)
 
         # The sun_position_PT CSV has three columns: time, elevation (elev), and azimute (azim)
-        df2 = pd.read_csv(file2_path) 
+        df2 = pd.read_csv(file2_path)
         logging.info('Read the sun_Position_PT dataset as dataframe')
 
-        df2.iloc[0:35041,1:] # The sun_position_PT file has 1 more day, so we have to filter it and also filter the time column
+        df2 = df2.iloc[0:35041,1:]  # The sun_position_PT file has 1 more day, so we have to filter it and also filter the time column
         df_merged = df_1.join(df2)
 
         logging.info('Ingestion of the data is completed')
         return df_merged
-    
+
     except Exception as e:
-        raise CustomException(e,sys)
-    
-# if __name__ == "__main__":
-#     file1_path = os.path.join('Dados\zKFdRou77JuivhIm.csv')
-#     file2_path = os.path.join('Dados\sun_position_PT.csv')
-    
-#     df_result = data_ingestion(file1_path, file2_path)
+        raise CustomException(e, sys)
--- src/components/main.py
+++ src/components/main.py
@@ -1,36 +1,32 @@
-import os
-import sys
 import glob as glob
 from src.components.data_ingestion import data_ingestion
 from src.components.data_preprocess import *
 from src.components.feature_engineering import *
 from src.components.model_logic import forecasting_logic
 from src.components.model_trainer import *
-from src.exception import CustomException
-from src.logger import logging
+import argparse
+
+"""
+This is our model's entry point
+Execution:
+python <path_to_this_file>/main.py \
+--files_dir 'C:\Users\emparrac\Desktop\Emanuel\Courses Data Science\My_Project_EDP_Data\Dados_EDP\anonimized_3' \
+--files2_path 'C:\Users\emparrac\Desktop\Emanuel\Courses Data Science\My_Project_EDP_Data\Dados_EDP\sun_position_PT.csv'
+"""
 
 
-# Main program
-files_dir=r"C:\Users\emparrac\Desktop\Emanuel\Courses Data Science\My_Project_EDP_Data\Dados_EDP\anonimized_3"
-file2_path =r'C:\Users\emparrac\Desktop\Emanuel\Courses Data Science\My_Project_EDP_Data\Dados_EDP\sun_position_PT.csv'
-
-#Verificar ficheiros csv na pasta
-os.chdir(files_dir) 
-filelist = glob.glob("*.csv")
-
-def main_function(filelist):
-
-    evaluation_metrics = {'RMSE':[], 'R2_SCORE':[]}
+def main(filelist):
+    evaluation_metrics = {'RMSE': [], 'R2_SCORE': []}
     df_evaluation_metrics = pd.DataFrame(evaluation_metrics)
-    
-    for count,filename in enumerate (filelist):    
-        #print(filename)
-        df = data_ingestion(filename,file2_path)
+
+    for count, filename in enumerate(filelist):
+        # print(filename)
+        df = data_ingestion(filename, file2_path)
 
         # validar files com produções nulas - file 167 com produção anual = 0
-        if df.production_PV.mean()==0:
+        if df.production_PV.mean() == 0:
             continue
-            
+
         interpolation = interpolation_nan(df)
         data_variables = time_variables(interpolation)
         data_resample = resample(data_variables)
@@ -39,16 +35,31 @@ def main_function(filelist):
         ft_eng3 = feat_eng3(ft_eng2)
         X, y = forecasting_logic(ft_eng3, frequency=24, days_lookback=14)
         X_train, y_train, X_val, y_val, X_test, y_test = split_train_val_test(X, y)
-        X_train_norm, X_val_norm, X_test_norm, y_train_norm, y_val_norm, y_test_norm, scaler_y = data_normalization(X_train, y_train, X_val, y_val, X_test, y_test)
+        X_train_norm, X_val_norm, X_test_norm, y_train_norm, y_val_norm, y_test_norm, scaler_y = data_normalization(
+            X_train, y_train, X_val, y_val, X_test, y_test)
         model = LSTM_model(X_train_norm, X_val_norm, y_train_norm, y_val_norm)
         prct_rmse, prct_r2_square = evaluate_model(model, X_test_norm, y_test_norm, scaler_y)
-        
+
         # Create a new DataFrame with RMSE and R2_SCORE values
         result_df = pd.DataFrame({'RMSE': [prct_rmse], 'R2_SCORE': [prct_r2_square]})
-        
+
         # Append the new DataFrame to df_evaluation_metrics
         df_evaluation_metrics = df_evaluation_metrics.append(result_df, ignore_index=True)
         df_evaluation_metrics = df_evaluation_metrics.reset_index()
-        df_evaluation_metrics = df_evaluation_metrics.drop(['index'],axis=1)
-        
+        df_evaluation_metrics = df_evaluation_metrics.drop(['index'], axis=1)
+
     return df_evaluation_metrics
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Time Series Forecast Model Template')
+    parser.add_argument('--files_dir', '-f', help='Data path', required=True)
+    parser.add_argument('--file2_path', '-fp', help='Data path', required=True)
+
+    args = parser.parse_args()
+
+    filelist = glob.glob(f'{args.files_dir}/*.csv')
+    file2_path = args.file2_path
+
+    main(filelist)
